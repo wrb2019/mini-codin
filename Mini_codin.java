5c46f6dcbeb0ee2a0bb1fcf45e959d65163d8a05
wrb2019 <2411018519@qq.com>
2020-02-11 22:01:43 +0800

    Create Mini_codin.java

---
+++ Mini_codin.java
@@ -0,0 +1,44 @@
+ private String digits;
+        private List<String> result;
+        private StringBuilder stringBuilder;
+        private static Map<Integer,String> map = new HashMap<>();
+        public List<String> letterCombinations(String digits) {
+            mapDataInit(map);
+            this.digits = digits;
+            result = new ArrayList<>();
+            if(digits.length() == 0) {
+                return result;
+            }
+            stringBuilder = new StringBuilder();
+            combination(0);
+            return result;
+        }
+
+        private static void mapDataInit(Map<Integer,String> map){
+            map.put(0, "");
+            map.put(1, "");
+            map.put(2, "ABC");
+            map.put(3, "DEF");
+            map.put(4, "GHI");
+            map.put(5, "JKL");
+            map.put(6, "MNO");
+            map.put(7, "PQRS");
+            map.put(8, "TUV");
+            map.put(9, "WXYZ");
+        }
+
+        private void combination(int currentIndex) {
+            if(currentIndex == digits.length()) {
+                result.add(stringBuilder.toString().toLowerCase());
+                return;
+            }
+            int index = digits.charAt(currentIndex)-'0';
+            if("".equals(map.get(index))){
+                currentIndex++;
+            }
+            for(char c: map.get(index).toCharArray()) {
+                stringBuilder.append(c);
+                combination(currentIndex + 1);
+                stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            }
+        }
